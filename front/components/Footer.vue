9665a296d108304ac127965eccaf526d9f21a935
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2022-11-27 18:41:58 +0100

    Init a Nuxt project

---
+++ front/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="p-4 self-end bg-white h-fit content-center w-full">
+        <p class="text-sm text-center font-sans">
+            Copyright © 2022 Alexis Balayre
+        </p>
+    </footer>
+</template>
---
+++ front/nuxt.config.ts
@@ -0,0 +1,31 @@
+export default defineNuxtConfig({
+    app: {
+        pageTransition: { name: 'page', mode: 'out-in' },
+        head: {
+            title: 'portfolio',
+            htmlAttrs: {
+              lang: 'en'
+            },
+            meta: [
+              { charset: 'utf-8' },
+              { name: 'viewport', content: 'width=device-width, initial-scale=1 viewport-fit=cover' },
+              { hid: 'description', name: 'description', content: '' },
+              { name: 'format-detection', content: 'telephone=no' }
+            ],
+            link: [
+              { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            ]
+        },
+    },
+
+    css: ['~/assets/css/main.css'],
+
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+
+
+})
