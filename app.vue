14e60b2b1d1fc9d6737734c145f2907fc1969006
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2022-12-04 20:17:19 +0100

    last version

--- app.vue
+++ app.vue
@@ -80,7 +80,7 @@
           <p class="lg:text-xl md:text-xl text-lg text-left font-medium mt-4">2019 - 2021 : Exploring Web2 technologies during my digital engineering studies</p>
         </div>
       </div>
-      <div class="md:mt-10 lg:mt-0 bg-white grid p-6 md:p-8 lg:p-8 mr-8 ml-8 mt-10 md:mt-4 rounded-xl lg:w-full lg:ml-20 lg:mr-20 md:ml-20 md:mr-20">
+      <div class="md:mt-10 md:mb-10 lg:mt-0 bg-white grid p-6 md:p-8 lg:p-8 mr-8 ml-8 mt-10 md:mt-4 rounded-xl lg:w-full lg:ml-20 lg:mr-20 md:ml-20 md:mr-20">
         <p class="lg:text-3xl text-2xl font-bold text-center">Services</p>
         <div>
           <p class="lg:text-xl md:text-xl text-lg text-left font-medium mt-4 md:mt-0"><a class="lg:text-xl md:text-xl text-lg  font-bold">Creation</a> and <a class="lg:text-xl md:text-xl text-lg  font-bold">deployment</a> of smart contracts</p>
@@ -123,7 +123,7 @@
       </div>
     </div>
 
-    <div class="mt-10 grid bg-indigo-200/30 ml-8 mr-8 lg:w-3/6 h-fit mb-10 lg:mb-20 p-6 md:p-8 rounded-xl justify-self-center">
+    <div class="md:mt-20 mt-10 grid bg-indigo-200/30 ml-8 mr-8 lg:w-3/6 h-fit mb-5 md:mb-10 p-6 md:p-8 rounded-xl justify-self-center">
       <ModalTxProgress class="hidden absolute left-1/2 transform -translate-x-1/2 -translate-y-1/4" id="modal" />
       <p class="lg:text-3xl text-2xl font-bold text-center ">Receive a free NFT</p>
       <p class="lg:text-xl md:text-xl text-lg mt-4 font-medium text-left">Enter your Ethereum wallet address</p>
