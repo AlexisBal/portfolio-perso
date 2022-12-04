19f05f2f0190a472db08cb7478ab2ac0ac69a757
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2022-12-04 12:09:35 +0100

    last version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,12 @@ export default defineNuxtConfig({
               { charset: 'utf-8' },
               { name: 'viewport', content: 'width=device-width, initial-scale=1 viewport-fit=cover' },
               { hid: 'description', name: 'description', content: 'Alexis Balayre - Full Stack Web 3 Developer' },
-              { name: 'format-detection', content: 'telephone=no' }
+              { name: 'format-detection', content: 'telephone=no' },
+              { name: 'og:type', content: 'website' },
+              { name: 'og:site_name', content: 'Alexis Balayre' },
+              { name: 'og:title', content: 'Alexis Balayre' },
+              { name: 'og:description', content: 'Alexis Balayre - Full Stack Web 3 Developer' },
+              { name: 'og:image', content: '/preview.png' },
             ],
             link: [
               { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -21,7 +26,7 @@ export default defineNuxtConfig({
             ]
         },
     },
-    
+
     css: ['~/assets/css/main.css'],
 
     postcss: {
