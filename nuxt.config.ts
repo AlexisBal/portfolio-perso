b3765eeaaf8ffc643c59b5de52e8d29237570598
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2022-12-04 11:49:10 +0100

    last version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,14 +9,22 @@ export default defineNuxtConfig({
             meta: [
               { charset: 'utf-8' },
               { name: 'viewport', content: 'width=device-width, initial-scale=1 viewport-fit=cover' },
-              { hid: 'description', name: 'description', content: 'Full Stack Web3 Developer' },
+              { hid: 'description', name: 'description', content: 'Alexis Balayre - Full Stack Web 3 Developer' },
               { name: 'format-detection', content: 'telephone=no' }
             ],
             link: [
-              { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+              { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+              { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+              { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
+              { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' },
+              { rel: 'manifest', href: '/site.webmanifest' }
             ]
         },
     },
+
+    modules: [
+      '@nuxtjs/sitemap'
+    ],
 
     css: ['~/assets/css/main.css'],
 
