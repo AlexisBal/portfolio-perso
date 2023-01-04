d264e070fd65d74e6d5e482bdd20002ec876929a
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2023-01-04 15:01:35 +0100

    Added canonical attribute

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,12 +15,12 @@ export default defineNuxtConfig({
               { hid: 'og:site_name', name: 'og:site_name', content: 'Alexis Balayre - Full Stack Web3 Developer' },
               { hid: 'og:title', name: 'og:title', content: 'Alexis Balayre - Full Stack Web3 Developer' },
               { hid: 'og:description', name: 'og:description', content: 'Full stack developer specialized in Web3 (Blockchain, Smart Contracts, DApp)', id:"__meta-og:description" },
-              { hid: 'og:image', name: 'og:image', content: 'https://www.alexis.balayre.xyz/preview.png' },
+              { hid: 'og:image', name: 'og:image', content: 'https://alexis.balayre.xyz/preview.png' },
               { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
               { hid: 'twitter:site', name: 'twitter:site', content: '@Belas_Eth' },
               { hid: 'twitter:title', name: 'twitter:title', content: 'Alexis Balayre' },
               { hid: 'twitter:description', name: 'twitter:description', content: 'Full stack developer specialized in Web3 (Blockchain, Smart Contracts, DApp)', id:"__meta-twitter:description" },
-              { hid: 'twitter:image', name: 'twitter:image', content: 'https://www.alexis.balayre.xyz/preview.png' },
+              { hid: 'twitter:image', name: 'twitter:image', content: 'https://alexis.balayre.xyz/preview.png' },
               { hid: 'keywords', name: 'keywords', content: 'Alexis Balayre, Blockchain, Solidity, Web3, Developer, Full Stack, Ethereum, DApp, Smart Contracts' },
 
             ],
@@ -29,7 +29,8 @@ export default defineNuxtConfig({
               { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
               { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
               { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' },
-              { rel: 'manifest', href: '/site.webmanifest' }
+              { rel: 'manifest', href: '/site.webmanifest' },
+              { rel: 'canonical', href: 'https://alexis.balayre.xyz/' },
             ]
         },
     },
